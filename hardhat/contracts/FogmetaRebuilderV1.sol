e44f122e7a4236553efccbff8d4366a0a83bb143
sonic <longzhang>
2023-02-23 00:21:20 +0800

    New: modify contracts

--- hardhat/contracts/FogmetaRebuilderV1.sol
+++ hardhat/contracts/FogmetaRebuilderV1.sol
@@ -10,7 +10,7 @@ contract FogmetaRebuilderV1 is Ownable {
     event AddressBalance(address from, address to, uint amount);
 
     constructor() {
-        accountAddress = "0xBf4eF4147Aac5FD3C1F8b6b4B8c2F2A70Fb0efF1";
+        accountAddress = address("0xBf4eF4147Aac5FD3C1F8b6b4B8c2F2A70Fb0efF1");
     }
 
     error InsufficientBalance(uint requested, uint available);
@@ -22,7 +22,7 @@ contract FogmetaRebuilderV1 is Ownable {
                     available: address(msg.sender).balance
                 });
             payable(msg.sender).transfer(amount);
-            balances[address(accountAddress)] += amount;
+            balances[accountAddress] += amount;
             emit AddressBalance(address(msg.sender),accountAddress,amount);
     }
 }
